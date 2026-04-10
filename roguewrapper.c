462215c322c70cba4e12df610c91b6b38982cd71
jackfisselbrand <jackfisselbrand@proton.me>
2026-04-10 16:23:14 -0400

    initial commit

---
+++ roguewrapper.c
@@ -0,0 +1,151 @@
+#include "roguewrapper.h"
+#include "rogueutil.h"
+
+char screen_char[TERMINAL_HEIGHT][TERMINAL_WIDTH] = {0};
+char screen_fgcolor[TERMINAL_HEIGHT][TERMINAL_WIDTH][10] = {0};
+char screen_bgcolor[TERMINAL_HEIGHT][TERMINAL_WIDTH][10] = {0};
+
+int terminal_x1;
+int terminal_y1;
+int print_offsetx;
+int print_offsety;
+int cursor_visibility;
+
+
+
+void setPrintOffsetX(int o) {
+	print_offsetx = o;
+}
+int getPrintOffsetX(int o) {
+	return print_offsetx;
+}
+void setPrintOffsetY(int o) {
+	print_offsety = o;
+}
+int getPrintOffsetY(int o) {
+	return print_offsety;
+}
+
+void setCursorVis(int v) {
+	cursor_visibility = v;
+	setCursorVisibility(v);
+	return;
+}
+int getCursorvis(void) {
+	return cursor_visibility;
+}
+
+int getTerminalRows(void) {
+	return trows();
+}
+int getTerminalCols(void) {
+	return tcols();
+}
+
+
+
+void setX1Pos(int x) {
+	terminal_x1 = x;
+	return;
+}
+int getX1Pos(void) {
+	return terminal_x1;
+}
+void setY1Pos(int y) {
+	terminal_y1 = y;
+	return;
+}
+int getY1Pos(void) {
+	return terminal_y1;
+}
+
+
+
+void setCharacter(char c) {
+	screen_char[terminal_x1][terminal_y1] = c;
+	return;
+}
+char getCharacter(void) {
+	return screen_char[terminal_x1][terminal_y1];
+}
+
+
+
+void setFgColor(int c) {
+	const char* fgColor = getANSIColor(c);
+	for (int i=0; i<strlen(fgColor); i++) {
+		screen_fgcolor[terminal_x1][terminal_y1][i] = fgColor[i];
+	}
+
+	return;
+}
+char* getFgColor(void) {
+	return screen_fgcolor[terminal_x1][terminal_y1];
+}
+
+void setBgColor(int c) {
+	const char* bgColor = getANSIBgColor(c);
+	for (int i=0; i<strlen(bgColor); i++) {
+		screen_bgcolor[terminal_x1][terminal_y1][i] = bgColor[i];
+	}
+
+	return;
+}
+char* getBgColor(void) {
+	return screen_bgcolor[terminal_x1][terminal_y1];
+}
+
+
+
+char getKeyPress(void) {
+	return nb_getch();
+}
+
+
+
+void print(void) {
+	int x = terminal_x1 + print_offsetx;
+	int y = terminal_y1 + print_offsety;
+
+	gotoxy(x, y);
+	printf("%s", screen_bgcolor[terminal_x1][terminal_y1]);
+
+	gotoxy(x, y);
+	printf("%s", screen_fgcolor[terminal_x1][terminal_y1]);
+
+	gotoxy(x, y);
+	printf("%c", screen_char[terminal_x1][terminal_y1]);
+	
+	terminal_x1++;
+	return;
+}
+void stamp(void) {
+	int x = terminal_x1 + print_offsetx;
+	int y = terminal_y1 + print_offsety;
+
+	gotoxy(x, y);
+	printf("%s", screen_bgcolor[terminal_x1][terminal_y1]);
+
+	gotoxy(x, y);
+	printf("%s", screen_fgcolor[terminal_x1][terminal_y1]);
+
+	gotoxy(x, y);
+	printf("%c", screen_char[terminal_x1][terminal_y1]);
+	gotoxy(x, y);
+
+	return;
+}
+
+
+
+void clear(void) {
+	cls();
+	return;
+}
+void clearColor(void) {
+	resetColor();
+	return;
+}
+void resetCursorVis(void) {
+	setCursorVis(1);
+}
---
+++ roguewrapper.h
@@ -0,0 +1,44 @@
+#ifndef ROGUEWRAPPER_H
+#define ROGUEWRAPPER_H
+
+#include <stdio.h>
+#include <string.h>
+
+#define TERMINAL_WIDTH 80
+#define TERMINAL_HEIGHT 24
+
+void setPrintOffsetX(int o);
+int getPrintOffsetX(int o);
+void setPrintOffsetY(int o);
+int getPrintOffsetY(int o);
+
+void setCursorVis(int v);
+int getCursorvis(void);
+
+int getTerminalRows(void);
+int getTerminalCols(void);
+
+void setX1Pos(int x);
+int getX1Pos(void);
+void setY1Pos(int y);
+int getY1Pos(void);
+
+void setCharacter(char c);
+char getCharacter(void);
+
+void setFgColor(int c);
+char* getFgColor(void);
+
+void setBgColor(int c);
+char* getBgColor(void);
+
+char getKeyPress(void);
+
+void print(void);
+void stamp(void);
+
+void clear(void);
+void clearColor(void);
+void resetCursorVis(void);
+
+#endif
